1c1481f38f046a2cc29788d14f1d94e3726169be
Henrique Lopes <h.lopes.silva2015@gmail.com>
2023-03-15 22:47:43 -0300

    Add - Description in Home

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import { Icon } from '../../atoms/Icons'
 import { Welcome } from '../../molecules/Welcome/indesx'
 import * as S from './styles'
 
@@ -5,6 +6,17 @@ export function PageHome() {
   return (
     <S._Container className="grid grid-cols-4 gap-4">
       <Welcome className="col-span-4" />
+
+      <S._Description className="col-span-4">
+        {/* <Icon/> */}
+        <p>
+          Sou desenvolvedor a um pouco mais de 3 anos, hoje invisto a maior
+          parte do meu tempo em estudos de tecnologias voltadas ao front end,
+          além de reforçar minhas soft-skills. Acredito que com esforço e foco
+          se obtém tudo!
+        </p>
+        <div className="description_background" />
+      </S._Description>
     </S._Container>
   )
 }
--- src/components/pages/home/styles.tsx
+++ src/components/pages/home/styles.tsx
@@ -1,3 +1,24 @@
 import styled from 'styled-components'
 
 export const _Container = styled.div``
+
+export const _Description = styled.div`
+  height: 50rem;
+  padding: 10rem 0;
+  display: flex;
+  align-items: center;
+
+  p {
+    color: #fff;
+    z-index: 1;
+  }
+
+  .description_background {
+    background: ${({ theme }) => theme.colors.violet[500]};
+    height: 40rem;
+    transform: skew(0deg, -0.18rad);
+    position: absolute;
+    width: 100%;
+    z-index: 0;
+  }
+`
