f18686c2431df4695d025f30bf4a9377dbbf1cf7
Henrique Lopes <39716479+Henrique0498@users.noreply.github.com>
2023-03-17 00:47:34 -0300

    Update - Component Button hover and focus

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,10 +1,6 @@
 import * as S from './styles'
 import { TypeButton } from './types'
 
-
-
-export function Button ({children, ...props}: TypeButton) {
-  return (
-    <S._Container {...props}>{children}</S._Container>
-  )
+export function Button({ children, ...props }: TypeButton) {
+  return <S._Container {...props}>{children}</S._Container>
 }
--- src/components/atoms/Button/styles.ts
+++ src/components/atoms/Button/styles.ts
@@ -2,9 +2,19 @@ import styled from 'styled-components'
 
 export const _Container = styled.button`
   align-items: center;
-  border: 2px solid ${({theme}) => theme.colors.blue[500]};
+  background: linear-gradient(
+    90deg,
+    rgba(0, 138, 255, 0) 0%,
+    rgba(0, 138, 255, 0) 50%,
+    ${({ theme }) => theme.colors.blue[600]} 50%,
+    ${({ theme }) => theme.colors.blue[600]} 100%
+  );
+  background-position-x: 0%;
+  background-position-y: center;
+  background-size: 200%, 120%;
+  border: 1px solid ${({ theme }) => theme.colors.blue[600]};
   border-radius: 0.25rem;
-  color: ${({theme}) => theme.colors.blue[500]};
+  color: ${({ theme }) => theme.colors.blue[600]};
   cursor: pointer;
   display: flex;
   font-size: 1rem;
@@ -12,5 +22,16 @@ export const _Container = styled.button`
   justify-content: center;
   line-height: 1.25rem;
   padding: 0.75rem 1.5rem;
+  position: relative;
   text-transform: uppercase;
+  transition: all 200ms;
+
+  :hover,
+  :focus {
+    background-position-x: 100%;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.blue[300]};
+    color: ${({ theme }) => theme.colors.blue[50]};
+    outline-offset: -1px;
+    outline: 1px solid ${({ theme }) => theme.colors.blue[600]};
+  }
 `
