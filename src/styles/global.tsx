2032af6f7417f4979097b3f55b487227b32a8b8b
Henrique Lopes <39716479+Henrique0498@users.noreply.github.com>
2023-03-17 00:48:38 -0300

    Fix - global front weight

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     font-size: 1rem;
-    font-weight: 500;
+    font-weight: 400;
     line-height: 1.5rem;
     font-family: Roboto, Ubuntu, -apple-system, BlinkMacSystemFont, Segoe UI, Oxygen, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
