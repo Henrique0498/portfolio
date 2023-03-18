a74fca96978d7f22bc32c9f2f9d9c2dcab8c10c3
Henrique Lopes <39716479+Henrique0498@users.noreply.github.com>
2023-03-18 15:28:02 -0300

    Change - component Icon

--- src/components/atoms/Icons/files.tsx
+++ src/components/atoms/Icons/files.tsx
@@ -9,7 +9,31 @@ export const IconsFiles = {
     >
       <path
         d="M8.84418e-06 73.6854L412 0V326.315L0 400L8.84418e-06 73.6854Z"
-        fill="#8B5CF6"
+        fill="currentColor"
+      />
+    </svg>
+  ),
+  quotationMarks: (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        width="22.9667"
+        height="4.80838"
+        rx="2.40419"
+        transform="matrix(0.751344 -0.659911 0.539457 0.842013 0 15.156)"
+        fill="currentColor"
+      />
+      <rect
+        width="22.9667"
+        height="4.80838"
+        rx="2.40419"
+        transform="matrix(0.751344 -0.659911 0.539457 0.842013 4.15019 19.9513)"
+        fill="currentColor"
       />
     </svg>
   )
