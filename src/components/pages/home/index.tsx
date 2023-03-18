a7e5c59a2577f3f580e847c4fdd2edf742fac7ab
Henrique Lopes <39716479+Henrique0498@users.noreply.github.com>
2023-03-18 15:28:32 -0300

    Change - description

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,21 +1,40 @@
+import Link from 'next/link'
 import { Icon } from '../../atoms/Icons'
 import { Welcome } from '../../molecules/Welcome/indesx'
 import * as S from './styles'
 
 export function PageHome() {
+  const timeExperience = new Date().getFullYear() - 2019
+
   return (
     <S._Container className="grid grid-cols-4 gap-4">
       <Welcome className="col-span-4" />
 
       <S._Description className="col-span-4">
-        {/* <Icon/> */}
-        <p>
-          Sou desenvolvedor a um pouco mais de 3 anos, hoje invisto a maior
-          parte do meu tempo em estudos de tecnologias voltadas ao front end,
-          além de reforçar minhas soft-skills. Acredito que com esforço e foco
-          se obtém tudo!
-        </p>
-        <div className="description_background" />
+        <Icon
+          icon="backgroundDiagonal"
+          className="description_background text-gray-900"
+        />
+        <div className="description_decorators">
+          <span>Quem é</span>
+          <span>Henrique Lopes</span>
+        </div>
+        <div className="description_container">
+          <Icon icon="quotationMarks" className="container_quotationMarks" />
+          <p>
+            &nbsp;&nbsp;Sou desenvolvedor a um pouco mais de
+            <b> {timeExperience} anos</b>, graduado em Gestão de TI na faculdade
+            Flamingo, e atualmente fazendo graduação de ADS na{' '}
+            <Link href="https://www.fiap.com.br/" target="_blank">
+              FIAP
+            </Link>
+            .<br />
+            &nbsp;&nbsp;Hoje invisto a maior parte do meu tempo estudando
+            tecnologias voltadas ao <b>Front-End</b>, além de reforçar minhas
+            <b> soft-skills</b>.<br />
+            Acredito que com esforço e foco se obtém tudo!
+          </p>
+        </div>
       </S._Description>
     </S._Container>
   )
--- src/components/pages/home/styles.tsx
+++ src/components/pages/home/styles.tsx
@@ -3,22 +3,81 @@ import styled from 'styled-components'
 export const _Container = styled.div``
 
 export const _Description = styled.div`
-  height: 50rem;
-  padding: 10rem 0;
   display: flex;
+  height: 32rem;
   align-items: center;
-
-  p {
-    color: #fff;
-    z-index: 1;
-  }
+  position: relative;
+  padding: 0 1rem;
+  overflow: hidden;
 
   .description_background {
-    background: ${({ theme }) => theme.colors.violet[500]};
-    height: 40rem;
-    transform: skew(0deg, -0.18rad);
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    left: 0;
+    overflow: hidden;
     position: absolute;
+    top: 0;
+    width: 100vw;
+    z-index: 1;
+
+    svg {
+      height: 100%;
+      min-width: 100vw;
+      position: absolute;
+      width: fit-content;
+    }
+  }
+
+  .description_decorators {
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
     width: 100%;
-    z-index: 0;
+    padding: 7rem 0.25rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    z-index: 2;
+
+    span {
+      display: block;
+      font-size: 3rem;
+      line-height: 3.75rem;
+      color: ${({ theme }) => theme.colors.gray[800]};
+      font-weight: 500;
+
+      :first-child {
+        align-self: flex-end;
+      }
+
+      :last-child {
+        align-self: flex-start;
+      }
+    }
+  }
+
+  .description_container {
+    z-index: 3;
+    position: relative;
+
+    .container_quotationMarks {
+      left: -0.25rem;
+      position: absolute;
+      top: -0.5rem;
+      z-index: 1;
+      color: ${({ theme }) => theme.colors.gray[800]};
+    }
+
+    p {
+      color: #fff;
+      z-index: 2;
+      position: relative;
+
+      b {
+        color: ${({ theme }) => theme.colors.purple[500]};
+      }
+    }
   }
 `
