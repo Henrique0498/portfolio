2cd602aa464a4c982de8ddeea9dd1a8fa742720b
Henrique Lopes <h.lopes.silva2015@gmail.com>
2023-03-21 20:46:51 -0300

    Change - Image card

--- src/components/organisms/CardProjectLarge/index.tsx
+++ src/components/organisms/CardProjectLarge/index.tsx
@@ -13,7 +13,7 @@ export const CardProjectLarge = () => {
           <Image
             className="background_project"
             alt="TESTE TESTE"
-            src="https://images.unsplash.com/photo-1679245883026-adea0f43e6f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
+            src="/images/card-background.jpg"
           />
 
           <Image
