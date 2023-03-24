05671b844f18a81697868dcc07e6c3ce0dd901fc
Henrique Lopes <39716479+Henrique0498@users.noreply.github.com>
2023-03-24 00:49:39 -0300

    Fix - Component Link

--- src/components/atoms/Link/styles.ts
+++ src/components/atoms/Link/styles.ts
@@ -3,17 +3,19 @@ import styled from 'styled-components'
 export const _Container = styled.span`
   color: ${({ theme }) => theme.colors.blue[600]};
   display: flex;
-  position: relative;
+  height: fit-content;
   justify-content: center;
+  position: relative;
+  width: fit-content;
 
   ::before {
-    content: '';
-    position: absolute;
     background-color: currentColor;
-    width: 0rem;
-    height: 0.126rem;
     bottom: 0rem;
+    content: '';
+    height: 0.126rem;
+    position: absolute;
     transition: all 200ms ease-in-out;
+    width: 0rem;
   }
 
   :hover {
