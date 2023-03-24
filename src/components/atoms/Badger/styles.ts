88a41d146c913d60daad8ee1e02b4f5a04755633
Henrique Lopes <39716479+Henrique0498@users.noreply.github.com>
2023-03-24 00:48:31 -0300

    Fix - Component Badger

--- src/components/atoms/Badger/styles.ts
+++ src/components/atoms/Badger/styles.ts
@@ -3,8 +3,10 @@ import { TypeComponentBadgerStyle } from './types'
 
 export const _Container = styled.div<TypeComponentBadgerStyle>`
   background-color: ${({ theme, color }) => theme.colors[color][300]};
-  color: ${({ theme, color }) => theme.colors[color][900]};
-  padding: 0.25rem 0.75rem;
-  ${({ theme }) => theme.text.body.sm}
   border-radius: 0.25rem;
+  color: ${({ theme, color }) => theme.colors[color][900]};
+  height: fit-content;
+  padding: 0.25rem 0.75rem;
+  width: fit-content;
+  ${({ theme }) => theme.text.body.sm}
 `
