10be50913aa20772456fbf8a294c8bb74f08df6c
Henrique Lopes <h.lopes.silva2015@gmail.com>
2023-03-30 23:36:36 -0300

    Add - MenuBurger

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from '../../atoms/Button'
 import { MenuBurger } from '../../atoms/MenuBurger/Index'
+
 import * as S from './styles'
 
 export function Header() {
   return (
     <S._Container>
-      <MenuBurger />
+      <MenuBurger aria-label="Meno do cabeçalho" className="hidden" />
       <Button aria-label="Baixar currículo">Currículo</Button>
     </S._Container>
   )
