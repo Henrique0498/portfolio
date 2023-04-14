3a6a9ecd5fb2f2a4532a0e76447517eb31d80e40
Henrique Lopes <h.lopes.silva2015@gmail.com>
2023-04-14 01:59:26 -0300

    Change - Types from Component Link

--- src/components/atoms/Link/types.ts
+++ src/components/atoms/Link/types.ts
@@ -4,5 +4,10 @@ export interface TypeComponentLink
   extends HtmlHTMLAttributes<HTMLAnchorElement> {
   children: string | React.ReactNode
   href: string
+  color?: string
   target?: '_blank' | '_parent' | '_self' | '_top'
+}
+
+export interface TypeComponentLinkStyled {
+  color: string
 }
