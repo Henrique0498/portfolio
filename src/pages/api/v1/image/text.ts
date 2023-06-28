dfcee1763c8473f7a2d7e95ea5e9c4eef460f31a
Henrique Lopes <h.lopes.silva2015@gmail.com>
2023-06-27 22:55:32 -0300

    FEATE: Add opacity

--- src/pages/api/v1/image/text.ts
+++ src/pages/api/v1/image/text.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 interface TypeFunctionGetSvg {
   text: string
   color: string
+  opacity: string
 }
 
 interface TypeFunctionMountBodySvg extends TypeFunctionGetSvg {
@@ -14,21 +15,27 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { text, color = 'rgb(0,0,0,0.04)' } = req.query
+  const { text, color = 'rgb(0,0,0,0.04)', opacity = '1' } = req.query
 
   if (typeof text !== 'string') {
     res.status(400).json({ message: 'text invalid' })
+  } else if (typeof color !== 'string') {
+    res.status(400).json({ message: 'color invalid' })
+  } else if (typeof opacity !== 'string') {
+    res.status(400).json({ message: 'opacity invalid' })
   } else {
     res.setHeader('Content-Type', 'image/svg+xml')
     res.setHeader(
       'Cache-Control',
       'public, immutable, no-transform, s-maxage=31536000, max-age=31536000'
     )
+    const modifiedColor = color.includes('rgb') ? color : '#' + color
 
     return res.end(
       getSvg({
-        color: color as string,
-        text: text as string
+        color: modifiedColor,
+        text,
+        opacity
       })
     )
   }
@@ -50,14 +57,15 @@ function getSvg(props: TypeFunctionGetSvg): string {
 
 function mountBodySvg({
   color,
-  text,
+  opacity,
   slice,
-  sliceOrderReverse
+  sliceOrderReverse,
+  text
 }: TypeFunctionMountBodySvg) {
   const repeat = slice === 0 ? 16 : 17
   const propsX = 5 * 0
   const propsY = slice === 0 ? '27%' : sliceOrderReverse ? '93%' : '60%'
-  const header = `<text x='${propsX}' y='${propsY}' fill='${color}' font-size='4rem' font-weight='600' font-family='Roobert,Helvetica Neue,Helvetica,Arial,sans-serif'>`
+  const header = `<text x='${propsX}' y='${propsY}' fill='${color}' opacity='${opacity}' font-size='4rem' font-weight='600' font-family='Roobert,Helvetica Neue,Helvetica,Arial,sans-serif'>`
   const footer = '</text>'
 
   const arrayWords = []
