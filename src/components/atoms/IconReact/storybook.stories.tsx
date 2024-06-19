d62d1283b7f4dba348a731d912b4e2fa1293268a
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-06-19 20:45:43 -0300

    Create: Component Icon React

---
+++ src/components/atoms/IconReact/storybook.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import IconReact from '.'
+
+export default {
+  title: 'atoms/IconReact',
+  component: IconReact
+} as Meta
+
+export const Default: StoryObj = {}
