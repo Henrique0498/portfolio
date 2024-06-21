86baf401c8f6ba3f56474615f195d01ebeb6c238
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-06-21 04:02:05 -0300

    Feat: Initial NextUI settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { nextui } from '@nextui-org/react'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
@@ -6,9 +7,10 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './stories/**/*.{js,ts,jsx,tsx}'
+    './stories/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {},
-  plugins: []
+  plugins: [nextui()]
 }
 export default config
