877f32014f8056fc56429fc5830679dffd1c841e
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-07-27 22:34:35 -0300

    Change: Welcome source

--- src/components/organisms/Welcome/storybook.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react'
-import Welcome from '.'
-
-export default {
-  title: 'organisms/Welcome',
-  component: Welcome
-} as Meta
-
-export const Default: StoryObj = {}
