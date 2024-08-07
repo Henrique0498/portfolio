c44fe6894db74657cf35bd62aa45ab90cad7cf6d
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-08-07 15:52:39 -0300

    Change: Footer

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,24 +1,46 @@
-import NavLink from '@/components/atoms/NavLink'
 import styles from './styles.module.scss'
+import IconLink from '@/components/atoms/IconLink'
 
 export default function Footer() {
+  const year = new Date().getFullYear()
+
   return (
     <footer className={styles.container}>
-      <nav>
-        <ul>
-          <li>
-            <NavLink href="https://www.linkedin.com/in/henrique-ls/">
-              LinkedIn
-            </NavLink>
-          </li>
-          <li>
-            <NavLink href="https://github.com/Henrique0498">Github</NavLink>
-          </li>
-          <li>
-            <NavLink href="/">Contato</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="container flex flex-col md:flex-row justify-between items-center gap-6">
+        <span className="order-3 md:order-1">{year} © Copyright</span>
+
+        <nav className="order-2 md:order-2">
+          <ul className="flex gap-4 flex-col sm:flex-row  text-gray-400">
+            <li>
+              <span className="line-through cursor-not-allowed">Projetos</span>
+            </li>
+            <li>
+              <span className="line-through cursor-not-allowed">Currículo</span>
+            </li>
+          </ul>
+        </nav>
+
+        <nav className="order-1 md:order-3">
+          <ul className="flex gap-4 text-2xl">
+            <li>
+              <IconLink
+                content="LinkedIn"
+                href="https://www.linkedin.com/in/henrique-ls/"
+                icon="simple-icons:linkedin"
+                target="_blank"
+              />
+            </li>
+            <li>
+              <IconLink
+                content="GitHub"
+                href="https://github.com/Henrique0498"
+                icon="simple-icons:github"
+                target="_blank"
+              />
+            </li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   )
 }
