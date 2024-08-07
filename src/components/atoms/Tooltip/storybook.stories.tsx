83d66e1291fe03bb7bff80500709c6f1b1687659
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-08-07 14:08:13 -0300

    Create: Component Tooltip

---
+++ src/components/atoms/Tooltip/storybook.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Tooltip from '.'
+
+export default {
+  title: 'atoms/Tooltip',
+  component: Tooltip
+} as Meta
+
+export const Default: StoryObj = {}
