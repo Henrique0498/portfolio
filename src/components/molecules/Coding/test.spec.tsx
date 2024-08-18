d8917512b1c926ae81343eecfed06a560710038e
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-08-17 23:43:23 -0300

    Change: Component coding

--- src/components/molecules/Coding/test.spec.tsx
+++ src/components/molecules/Coding/test.spec.tsx
@@ -2,13 +2,23 @@ import { render } from '@testing-library/react'
 
 import Coding from '.'
 import { Suspense } from 'react'
+import { ClientProviders } from '@/app/providers'
 
 describe('<Coding />', () => {
   it('should render the link', () => {
     const { container } = render(
-      <Suspense>
-        <Coding code="console.log('teste)" className="testing" />
-      </Suspense>
+      <ClientProviders>
+        <Suspense>
+          <Coding
+            code="console.log('teste)"
+            className="testing"
+            breadcrumbs=""
+            lang="abap"
+            theme="andromeeda"
+            title=""
+          />
+        </Suspense>
+      </ClientProviders>
     )
 
     expect(container.firstChild).toBeInTheDocument()
